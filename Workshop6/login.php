1fdbf61975fa35dcec392fd99f6fecfc54d5b665
Bladimir Arroyo <bladimir.ab@gmail.com>
2024-10-15 21:13:17 -0600

    Working on feature branch for login

--- Workshop6/login.php
+++ Workshop6/login.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
+<?php
+  $error_msg = isset($_GET['error']) ? $_GET['error'] : '';
+?>
+<?php require('inc/header.php')?>
+  <div class="container-fluid">
+    <div class="jumbotron">
+      <h1 class="display-4">Login</h1>
+      <p class="lead">User Login</p>
+      <hr class="my-4">
+    </div>
+    <form method="post" action="actions/login.php">
+      <div class="error">
+        <?php echo $error_msg; ?>
+      </div>
+      <div class="form-group">
+        <label for="email">Email Address</label>
+        <input id="email" class="form-control" type="text" name="username">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input id="password" class="form-control" type="password" name="password">
+      </div>
+      <button type="submit" class="btn btn-primary"> Login </button>
+    </form>
+  </div>
+<?php require('inc/footer.php');
